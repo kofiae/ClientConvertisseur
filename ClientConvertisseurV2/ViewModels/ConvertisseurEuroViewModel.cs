ca738dc3a8cc5ea7e3d0cadb8ba5957da152a506
Olubusola Odufejo Ogoe <olubusola.odufejoogoe@etu.univ-smb.fr>
2023-02-02 08:42:17 +0100

    Couches ViewModel et View

---
+++ ClientConvertisseurV2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -0,0 +1,115 @@
+﻿using ClientConvertisseurV2.Models;
+using ClientConvertisseurV2.Services;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConvertisseurV2.ViewModels
+{
+    public class ConvertisseurEuroViewModel : CommunityToolkit.Mvvm.ComponentModel.ObservableObject
+    {
+        #region properties
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double montantEuros;
+        private ObservableCollection<Devise> devises;
+        private Devise selectedDevise;
+        private double montantEnDevise;
+
+        public double MontantEuros
+        {
+            get
+            {
+                return montantEuros;
+            }
+
+            set
+            {
+                montantEuros = value;
+            }
+        }
+
+        public ObservableCollection<Devise> Devises
+        {
+            get
+            {
+                return devises;
+            }
+
+            set
+            {
+                devises = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public Devise SelectedDevise
+        {
+            get
+            {
+                return selectedDevise;
+            }
+
+            set
+            {
+                selectedDevise = value;
+            }
+        }
+
+        public double MontantEnDevise
+        {
+            get
+            {
+                return montantEnDevise;
+            }
+
+            set
+            {
+                montantEnDevise = value;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+
+        public ConvertisseurEuroViewModel()
+        {
+            GetDataOnLoadAsync();
+        }
+
+        private async void GetDataOnLoadAsync()
+        {
+            WSService service = new WSService("https://localhost:44353/api/");
+            List<Devise> result = await service.GetDevisesAsync("devises");
+            if (result == null)
+                DisplayDialog("Erreur", "API non disponible !");
+            else
+                Devises = new ObservableCollection<Devise>(result);
+        }
+
+        private async void DisplayDialog(string title, string content)
+        {
+            ContentDialog erreurDevise = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                CloseButtonText = "Ok"
+            };
+            //erreurDevise.XamlRoot = this.Content.XamlRoot;
+            ContentDialogResult result = await erreurDevise.ShowAsync();
+        }
+
+    }
+}
